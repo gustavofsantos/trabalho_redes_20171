b0e1611834ceb7d0f8f4e693dd3837ae124820d5
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2017-07-09 23:12:09 -0300

    Transmissão funcionando perfeitamente sem quebrar a execução

--- cliente.py
+++ cliente.py
@@ -11,6 +11,7 @@ import socket
 import pyaudio
 import wave
 
+
 def imp_msg(msg, tipo="aviso"):
 	"""
 	tipo: aviso, erro, sucesso
@@ -24,6 +25,111 @@ def imp_msg(msg, tipo="aviso"):
 	else:
 		imp_msg("Tipo de mensagem não identificado: {}".format(tipo), "erro")
 
+
+class Cliente():
+	def __init__(self, ip_servidor, porta_servidor):
+		self.ip_servidor = ip_servidor
+		self.porta_servidor = porta_servidor
+
+		imp_msg("Conectando com {}:{}".format(self.ip_servidor, self.porta_servidor))
+		self.socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket_cliente.connect((self.ip_servidor, self.porta_servidor))
+		imp_msg("Conectado com {}:{}".format(self.ip_servidor, self.porta_servidor), "sucesso")
+
+		self.musicas = []
+
+	def listar(self):
+		# comunica com o servidor
+		self.socket_cliente.send("listar".encode())
+		self.musicas = self.socket_cliente.recv(2048).decode().split(",")
+
+		print("### MÚSICAS DISPONÍVEIS ###")
+		cont = 0
+		for musica in self.musicas:
+			print("\t[{}] {}".format(cont, musica))
+			cont = cont + 1
+
+	def novo_usuario(self, usuario, senha):
+		msg = "novo:{},{}".format(usuario, senha)
+		self.socket_cliente.send(msg.encode())
+
+	def transmitir(self, musica):
+		# comunica com o servidor
+		try:
+			self.socket_cliente.send("transmitir:{}".format(musica).encode())
+
+			imp_msg("Solicitando a música {}...".format(musica))
+
+			# recebe informações da música
+			dados_musica = self.socket_cliente.recv(1024).decode()
+			bloco, formato, canais, taxa = dados_musica.split(",")
+
+			bloco = int(bloco)
+			formato = int(formato)
+			canais = int(canais)
+			taxa = int(taxa)
+
+			# envia o sinal para iniciar a transmissão
+			self.socket_cliente.send("ok".encode())
+
+			imp_msg("Abrindo canal para transmissão...")
+			pa = pyaudio.PyAudio()
+			stream = pa.open(format=formato, channels=canais, rate=taxa, output=True)
+
+			# espera pelo primeiro bloco de dados
+			dado = self.socket_cliente.recv(bloco)
+
+			# escreve os bytes no buffer de execução de arquivos wav
+			while len(dado) >= bloco:
+				stream.write(dado)
+				dado = self.socket_cliente.recv(bloco)
+				#print("Recebeu ", len(dado))
+
+			stream.stop_stream()
+			#stream.close()
+			#pa.close()
+			imp_msg("Transmissão finalizada", "sucesso")
+		except Exception as e:
+			imp_msg("Erro na transmissão.", "erro")
+
+	def executar(self):
+		loop_while = True
+		while loop_while:
+			input_cliente = input("> ")
+			
+			if input_cliente == "sair":
+				self.socket_cliente.send("sair".encode())
+				loop_while = False
+
+			elif input_cliente == "novo":
+				# cria novo usuário
+				imp_msg("Não implementado", "erro")
+				# usuario = input("Nome de usuario: ")
+				# senha = input("Senha do usuario: ")
+				# self.novo_usuario(usuario, senha)
+
+			elif input_cliente == "login":
+				imp_msg("Não implementado", "erro")
+
+			elif input_cliente == "listar":
+				self.listar()
+
+			elif input_cliente == "transmitir":
+				print("Entre com o ID da música (número):")
+				input_cliente = input("> ")
+				if len(self.musicas) > 0:
+					# testa se o cliente está selecionando uma música invalida
+					if 0 <= int(input_cliente) < len(self.musicas):
+						musica = self.musicas[int(input_cliente)]
+						self.transmitir(musica)
+					else:
+						imp_msg("Música selecionada não existe.", "erro")
+				else:
+					imp_msg("Cache de musicas está vazio, faça uma listagem primeiro", "erro")
+
+		self.socket_cliente.close()
+
+
 def main():
 	imp_msg("Endereço IP do servidor: ")
 	ip_servidor = input("> ")
@@ -31,45 +137,8 @@ def main():
 	imp_msg("Número de porta do servidor: ")
 	porta_servidor = int(input("> "))
 
-	imp_msg("Conectando com {}:{}".format(ip_servidor, porta_servidor))
-	socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket_cliente.connect((ip_servidor, porta_servidor))
-	imp_msg("Conectado com {}:{}".format(ip_servidor, porta_servidor), "sucesso")
-
-	loop_while = True
-	while loop_while:
-		input_cliente = input("> ")
-		socket_cliente.send(input_cliente.encode())
-		if input_cliente == "sair":
-			loop_while = False
-		elif input_cliente == "transmitir":
-			# recebe informações da música
-			dados_musica = socket_cliente.recv(1024).decode()
-			bloco, formato, canais, taxa = dados_musica.split(",")
-			
-			bloco = int(bloco)
-			formato = int(formato)
-			canais = int(canais)
-			taxa = int(taxa)
-
-			# envia o sinal para iniciar a transmissão
-			socket_cliente.send("ok".encode())
-
-			imp_msg("Abrindo canal para transmissão...")
-			pa = pyaudio.PyAudio()
-			stream = pa.open(format=formato, channels=canais, rate=taxa, output=True)
-
-			# espera pelo primeiro bloco de dados
-			dado = socket_cliente.recv(bloco)
-			while len(dado) != 0:
-				stream.write(dado)
-				dado = socket_cliente.recv(bloco)
-
-			stream.stop_stream()
-			stream.close()
-			pa.close()
-
-	socket_cliente.close()
+	cliente = Cliente(ip_servidor, porta_servidor)
+	cliente.executar()
 
 if __name__ == '__main__':
 	main()
--- servidor.py
+++ servidor.py
@@ -6,121 +6,192 @@
 =                                    2017                                     =
 =                                                                             =
 ========================== IMPLEMENTAÇÃO DO SERVIDOR ==========================
+
+    Cada cliente funciona como uma thread no servidor, assim é possível
+    aceitar diversos clientes conectados e efetuando transmissões cada
+
 """
 import socket
 import threading
 import pyaudio
 import wave
+import os
+
 
 def imp_msg(msg, tipo="aviso"):
-	"""
-	tipo: aviso, erro, sucesso
-	"""
-	if tipo == "aviso":
-		print("\n[!] {}".format(msg))
-	elif tipo == "erro":
-		print("\n[-] {}".format(msg))
-	elif tipo == "sucesso":
-		print("\n[+] {}".format(msg))
-	else:
-		imp_msg("Tipo de mensagem não identificado: {}\n".format(tipo), "erro")
+    """
+    tipo: aviso, erro, sucesso
+    """
+    if tipo == "aviso":
+        print("\n[!] {}".format(msg))
+    elif tipo == "erro":
+        print("\n[-] {}".format(msg))
+    elif tipo == "sucesso":
+        print("\n[+] {}".format(msg))
+    else:
+        imp_msg("Tipo de mensagem não identificado: {}\n".format(tipo), "erro")
 
 
 class Cliente(threading.Thread):
-	def __init__(self, cliente):
-		threading.Thread.__init__(self)
-		self.cliente = cliente
+    """Classe que implementa um cliente conectado ao servidor"""
+    def __init__(self, cliente):
+        threading.Thread.__init__(self)
+        self.cliente = cliente
+        self.conexao_cliente, self.end_cliente = self.cliente
 
-	def run(self):
-		"""método que roda a conexão com o cliente na thread"""	
-		bloco = 1024
+    def novo_usuario(self, usuario, senha):
+        # carregar a lista de usuarios
 
-		conexao_cliente, end_cliente = self.cliente
-		imp_msg("Novo cliente {}".format(end_cliente))
-		
-		loop_while = True
+        # insere o novo usuario
 
-		while loop_while:
-			input_cliente = conexao_cliente.recv(1024).decode()
-			imp_msg("${}: {}".format(end_cliente, input_cliente))
-			if input_cliente == "sair":
-				loop_while = False
-			elif input_cliente == "transmitir":
-				imp_msg("Iniciando transmissão...")
-				try:
-					arquivo = "musicas/Thunderstruck.wav"
-					w_arquivo = wave.open(arquivo, "rb")
-					pa = pyaudio.PyAudio()
+        # libera para fazer o login
+        pass
 
-					stream = pa.open(
-						format=pa.get_format_from_width(w_arquivo.getsampwidth()),
-						channels=w_arquivo.getnchannels(),
-						rate=w_arquivo.getframerate(),
-						output=True)
+    def login_usuario(self, usuario, senha):
+        # solicita a senha do usuario
 
-					# codifica as informações da musica
-					# bloco, formato, canais, taxa
-					info = "{},{},{},{}".format(bloco, pa.get_format_from_width(w_arquivo.getsampwidth()), w_arquivo.getnchannels(), w_arquivo.getframerate())
+        # carrega o ambiente do usuario
+        pass
 
-					# envia as informações da música
-					conexao_cliente.send(info.encode())
+    def listar_musicas_diretorio(self):
+        musicas = []
+        # lista as músicas do diretório músicas
+        for (d_caminho, d_nome, d_arquivos) in os.walk("musicas"):
+            for d_arquivo in d_arquivos:
+                musicas.append(d_arquivo.split('.')[0])
 
-					# espera pela confirmação do cliente
-					input_cliente = conexao_cliente.recv(1024).decode()
+        s_musicas = ",".join(musicas)
 
-					if input_cliente == "ok":
-						imp_msg("Transmitindo a musica {}".format(arquivo.split('/')[1]))
+        imp_msg("Comunicando com o cliente {}.".format(self.end_cliente))
+        self.conexao_cliente.send(s_musicas.encode())
+        imp_msg("{} bytes enviados para {}.".format(
+            len(s_musicas), self.end_cliente))
 
-						# primeira amostra
-						dado = w_arquivo.readframes(bloco)
+    def transmitir(self, musica):
+        bloco = 1024
+        imp_msg("Iniciando transmissão...")
+        try:
+            arquivo = musica
+            w_arquivo = wave.open(arquivo, "rb")
+            pa = pyaudio.PyAudio()
 
-						# envia o arquivo de audio
-						while dado:
-							conexao_cliente.send(dado)
-							dado = w_arquivo.readframes(bloco)
+            # abre o canal de transmissão
+            stream = pa.open(
+                format=pa.get_format_from_width(w_arquivo.getsampwidth()),
+                channels=w_arquivo.getnchannels(),
+                rate=w_arquivo.getframerate(),
+                output=True)
 
-						conexao_cliente.send(''.encode())
-					else:
-						imp_msg("Cliente não confirmou os dados da música para transmissão", "erro")
+            # codifica as informações da musica
+            # bloco, formato, canais, taxa
+            info = "{},{},{},{}".format(bloco, pa.get_format_from_width(
+                w_arquivo.getsampwidth()), w_arquivo.getnchannels(), w_arquivo.getframerate())
 
-				except Exception as e:
-					imp_msg("Erro na transmissão", "erro")
-					print(e)
+            # envia as informações da música
+            self.conexao_cliente.send(info.encode())
 
-		conexao_cliente.close()
-		imp_msg("Conexão fechada de {}".format(end_cliente))
+            # espera pela confirmação do cliente
+            input_cliente = self.conexao_cliente.recv(1024).decode()
+
+            # se o cliente confirmar os dados da transmissão, então ela é iniciada
+            if input_cliente == "ok":
+                imp_msg("Transmitindo a musica {} para {}.".format(
+                    arquivo.split('/')[1], self.end_cliente))
+
+                # primeira amostra
+                tell_i = w_arquivo.tell()
+                dado = w_arquivo.readframes(bloco)
+                bs = True
+
+                # envia o arquivo de audio
+                while bs:
+                    self.conexao_cliente.send(dado)
+                    tell_i = w_arquivo.tell()
+                    dado = w_arquivo.readframes(bloco)
+                    tell = w_arquivo.tell()
+                    if tell == tell_i:
+                        bs = False
+
+                self.conexao_cliente.send('a'.encode())
+                imp_msg("Transmissão de {} finalizada.".format(
+                    arquivo.split('/')[1]), "sucesso")
+            else:
+                imp_msg(
+                    "Cliente não confirmou os dados da música para transmissão", "erro")
+
+        except Exception as e:
+            imp_msg("Erro na transmissão", "erro")
+
+    def run(self):
+        """método que roda a conexão com o cliente na thread"""
+
+        imp_msg("Novo cliente {}".format(self.end_cliente))
+
+        loop_while = True
+
+        while loop_while:
+            input_cliente = self.conexao_cliente.recv(1024).decode()
+            imp_msg("${}: {}".format(self.end_cliente, input_cliente))
+
+            if len(input_cliente.split(":")) > 1:
+                comando, argumento = input_cliente.split(":")
+            else:
+                comando = input_cliente
+
+            if comando == "sair":
+                loop_while = False
+                # finalizar o cliente
+            elif comando == "login":
+                # executa o login do usuário
+                imp_msg("Não implementado", "erro")
+                # usuario, senha = argumento.split(",")
+                # self.login_usuario(usuario, senha)
+            elif comando == "novo":
+                imp_msg("Não implementado", "erro")
+                # usuario, senha = argumento.split(",")
+                # self.novo_usuario(usuario, senha)
+            elif comando == "listar":
+                # envia ao cliente uma listagem das musicas contidas no diretório de músicas
+                self.listar_musicas_diretorio()
+            elif comando == "transmitir":
+                # transmite a musica selecionada ao cliente que a requisitou
+                self.transmitir("musicas/{}.wav".format(argumento))
+
+        self.conexao_cliente.close()
+        imp_msg("Conexão fechada de {}".format(self.end_cliente))
 
 
 def main():
-	# encapsula o servidor dentro de um bloco de excessão para capturar erros
-	try:
-		# define o endereço do servidor
-		ip_servidor = socket.gethostbyname(socket.gethostname())
-		porta = 9999
+    # encapsula o servidor dentro de um bloco de excessão para capturar erros
+    try:
+        # define o endereço do servidor
+        ip_servidor = socket.gethostbyname(socket.gethostname())
+        porta = 9999
 
-		imp_msg("Servidor iniciando em {}:{}...".format(ip_servidor, porta))
+        imp_msg("Servidor iniciando em {}:{}...".format(ip_servidor, porta))
 
-		# cria o socket do servidor e o coloca para escutar
-		socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket_servidor.bind((ip_servidor, porta))
-		socket_servidor.listen(3)
+        # cria o socket do servidor e o coloca para escutar
+        socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_servidor.bind((ip_servidor, porta))
+        socket_servidor.listen(3)
 
-		imp_msg("Aguardando conexões. Pressione Ctrl+C para parar.", "sucesso")
+        imp_msg("Aguardando conexões. Pressione Ctrl+C para parar.", "sucesso")
 
-		while True:
-			conexao_cliente, end_cliente = socket_servidor.accept()
-			try:
-				if conexao_cliente != None:
-					t_cliente = Cliente((conexao_cliente, end_cliente))
-					t_cliente.start()
-					#t_cliente.join()
-				else:
-					print("deu ruim")
-			except:
-				imp_msg("Falha na conexão com o cliente {}".format(end_cliente), "erro")
-			
-	except KeyboardInterrupt as erro:
-		sys.exit(1)
+        while True:
+            conexao_cliente, end_cliente = socket_servidor.accept()
+            try:
+                if conexao_cliente != None:
+                    t_cliente = Cliente((conexao_cliente, end_cliente))
+                    t_cliente.start()
+                    # t_cliente.join()
+                else:
+                    print("deu ruim")
+            except:
+                imp_msg("Falha na conexão com o cliente {}".format(
+                    end_cliente), "erro")
+
+    except KeyboardInterrupt as erro:
+        sys.exit(1)
 
 if __name__ == '__main__':
-	main()
+    main()
